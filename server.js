6dc80feb003a5b870ba7ea9b20922fc18cad91cc
Yulun Wu <ywu509@hotmail.com>
2021-09-23 02:34:29 +1200

    Config server

--- server.js
+++ server.js
@@ -33,11 +33,12 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
     
 //Serve static assets if in production
 //Deploy
+process.env.NODE_ENV = 'production'
 if(process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'))
+    
     //axios.defaults.baseURL = process.env.SERVER_URI;
-    //require('./config/config');
     app.get ('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 
