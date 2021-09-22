56e21a61ec9b7c2548a73ae6db4651f494358382
Yulun Wu <ywu509@hotmail.com>
2021-09-23 02:22:52 +1200

    Config server deploy URI setting.

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ if(process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'))
     //axios.defaults.baseURL = process.env.SERVER_URI;
-    require('./config/config');
+    //require('./config/config');
     app.get ('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 
